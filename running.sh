d8b739629a31d8e734a56d4bdab953043b22ecc8
luckylucky421 <30818309+luckylucky421@users.noreply.github.com>
2019-06-03 18:39:52 +0800

    Update running.sh

--- running.sh
+++ running.sh
@@ -2,5 +2,5 @@ running=`kubectl get pods -n devlopment|grep Running|awk '{print $3}'`> /dev/nul
 if [ "$running" == "Running" ]; then
    echo "continue"
 else
-  kubectl delete -f k8s-dev.yaml
+  kubectl delete -f k8s-dev.yaml > /dev/null 2>&1
 fi
